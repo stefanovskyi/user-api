58f06ff23b439cfc5f2f3ce85bba30ad67115f93
Oleksandr Stefanovskyi <astefanovskiy@gmail.com>
2019-10-13 15:15:31 +0300

    Set up GET endpoint to retrieve data sorted

--- src/main/scala/com/stefanovskyi/userapi/UserApiRoutes.scala
+++ src/main/scala/com/stefanovskyi/userapi/UserApiRoutes.scala
@@ -3,18 +3,34 @@ package com.stefanovskyi.userapi
 import cats.effect.Sync
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
+import org.http4s.dsl.impl.QueryParamDecoderMatcher
 
 object UserApiRoutes {
+  object SortingParamMatcher extends QueryParamDecoderMatcher[String]("sort")
+
   def userRoutes[F[_]: Sync](H: UserService[F]): HttpRoutes[F] = {
     val dsl = new Http4sDsl[F]{}
     import dsl._
 
     HttpRoutes.of[F] {
-         case GET -> Root / "users" => Ok("Get All users")
-         case GET -> Root / "users" / id => Ok(s"Get user by $id")
-         case POST -> Root / "users" => Created("Create new user")
-         case PUT -> Root / "users" => Accepted("User has been edited")
-         case DELETE -> Root / "users" / id => Ok(s"Delete user by id: $id")
+
+      case GET -> Root / "users" :? SortingParamMatcher(sort) =>
+        Ok(s"Get all users sorted $sort")
+
+      case GET -> Root / "users" =>
+        Ok("Get all users")
+
+      case GET -> Root / "users" / id =>
+        Ok(s"Get user by $id")
+
+      case POST -> Root / "users" =>
+        Created("Create new user")
+
+      case PUT -> Root / "users" =>
+        Accepted("User has been edited")
+
+      case DELETE -> Root / "users" / id =>
+        Ok(s"Delete user by id: $id")
     }
   }
 }
